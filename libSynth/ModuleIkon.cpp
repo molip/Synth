84e8353ab158d597ec126cd897458dfeb34bc19f
Jon White <jxxwhite@gmail.com>
2017-03-03 20:31:32 +0000

    Nicer pins.

--- libSynth/ModuleIkon.cpp
+++ libSynth/ModuleIkon.cpp
@@ -3,8 +3,19 @@
 #include "Model/Module.h"
 #include "Model/ModuleTypes.h"
 
+#include <algorithm>
+
 using namespace Synth;
 using namespace Synth::UI;
+
+namespace
+{
+	const int PinWidth = 50;
+	const int PinHeight = 20;
+	const int PinGap = 10;
+	const int MinHeight = 50;
+	const int Width = 150;
+}
 
 int ModuleIkon::GetModuleID() const 
 {
@@ -13,8 +24,11 @@ int ModuleIkon::GetModuleID() const
 
 Model::Rect ModuleIkon::GetRect() const
 {
-	Model::Point pos = _module.GetPosition();
-	return Model::Rect(pos, pos + Model::Point(300, 200));
+	const Model::Point pos = _module.GetPosition();
+	const int pinCount = (int)std::max(_module.GetDef().GetOutputs().size(), _module.GetDef().GetInputs().size());
+	const int height = std::max(MinHeight, pinCount * (PinHeight + PinGap));
+
+	return Model::Rect(pos, pos + Model::Point(Width, height));
 }
 
 const std::string& ModuleIkon::GetName() const
@@ -31,16 +45,15 @@ std::vector<ModuleIkon::Pin> ModuleIkon::GetPins(bool outputs) const
 
 	if (!pinDefs.empty())
 	{
-		double unit = 200 / double(pinDefs.size());
-		double top = modRect.Top();
-		int left = outputs ? modRect.Right() : modRect.Left() - 50;
-		Model::Rect rect(left, 0, left + 50, 0);
+		const int left = outputs ? modRect.Right() - 1 : modRect.Left() + 1 - PinWidth;
+		const int top = modRect.Top() + (modRect.Height() - ((int)pinDefs.size() * (PinHeight + PinGap) - PinGap)) / 2;
+
+		Model::Rect rect(left, top, left + PinWidth, 0);
 		for (auto& pin : pinDefs)
 		{
-			rect.Top() = int(top);
-			rect.Bottom() = int(rect.Top() + unit);
+			rect.Bottom() = rect.Top() + PinHeight;
 			pins.push_back(Pin{pin.GetName(), rect});
-			top += unit;
+			rect.Top() += PinHeight + PinGap;
 		}
 	}
 

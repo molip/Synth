ff23265e75c4e910eb20dd391535b92610aa37e3
Jon White <jxxwhite@gmail.com>
2017-03-25 09:48:30 +0000

    Bug fix: now works with non-POT polyphony.

--- Engine/PolyMixerModule.cpp
+++ Engine/PolyMixerModule.cpp
@@ -9,7 +9,7 @@ PolyMixerModule::PolyMixerModule()
 void PolyMixerModule::Update()
 {
 	SignedMultiInput& multiInput = _signedMultiInputs[Pin::PolyMixer::SignedMultiInput::Signal];
-	uint32_t total = 0;
+	int32_t total = 0;
 	int count = multiInput.GetSize();
 	for (int i = 0; i < count; ++i)
 		total += multiInput[i].GetValue();

ad56727f5c40c14c6e0b9632aaa4106a9010ccc8
Jon White <jxxwhite@gmail.com>
2018-11-15 20:07:18 +0000

    Phase mod optimisation

--- Engine/OscillatorModule.cpp
+++ Engine/OscillatorModule.cpp
@@ -13,6 +13,7 @@ void OscillatorModule::Update()
 {
 	Input& levelInput = _inputs[Pin::Oscillator::Input::Level];
 	Input& pitchInput = _inputs[Pin::Oscillator::Input::Pitch];
+	Input& phaseModInput = _inputs[Pin::Oscillator::Input::PhaseMod];
 	Output& signalOutput = _outputs[Pin::Oscillator::Output::Signal];
 
 	const float level = levelInput.GetValue();
@@ -30,9 +31,11 @@ void OscillatorModule::Update()
 		_phaseDelta = ::PitchToPhaseDelta(pitchInput.GetValue());
 	}
 
+	const int16_t phaseMod = phaseModInput.IsConnected() ? static_cast<int16_t>(phaseModInput.GetValue() * 0x7fff) : 0;
+
 	_phase += _phaseDelta;
 	
-	uint16_t phase = _phase + static_cast<int16_t>(_inputs[Pin::Oscillator::Input::PhaseMod].GetValue() * 0x7fff);
+	uint16_t phase = _phase + phaseMod;
 	const byte waveform = static_cast<byte>(_inputs[Pin::Oscillator::Input::Waveform].GetValue());
 	uint16_t output = ::SampleWaveform(waveform, phase, 0x8000);
 

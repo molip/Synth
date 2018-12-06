a6426c86b767d4ecea0953a19c5624953b15be6d
Jon White <jxxwhite@gmail.com>
2018-12-06 09:33:29 +0000

    Value types for signal inputs

--- libSynth/Model/ModuleTypes.cpp
+++ libSynth/Model/ModuleTypes.cpp
@@ -29,7 +29,7 @@ ModuleTypes::ModuleTypes()
 	_types.back().AddInput(std::make_unique<PinType>("wave", "Waveform", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Input::Waveform)).SetValueType(std::make_unique<IntValueType>(0, 1, 0, 3));
 	_types.back().AddInput(std::make_unique<PinType>("ptch", "Pitch", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Input::Pitch)).SetValueType(std::make_unique<PitchValueType>());
 	_types.back().AddInput(std::make_unique<PinType>("levl", "Level", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Input::Level)).SetValueType(std::make_unique<PercentValueType>(0, 100, 0, 1000));
-	_types.back().AddInput(std::make_unique<PinType>("pmod", "PhaseMod", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Input::PhaseMod));
+	_types.back().AddInput(std::make_unique<PinType>("pmod", "PhaseMod", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Input::PhaseMod)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
 	_types.back().AddOutput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Oscillator::Output::Signal));
 
 	_types.emplace_back("pmix", "PolyMixer", Engine::ModuleType::PolyMixer);
@@ -37,33 +37,33 @@ ModuleTypes::ModuleTypes()
 	_types.back().AddOutput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::PolyMixer::Output::Signal));
 
 	_types.emplace_back("trgt", "Target", Engine::ModuleType::Target);
-	_types.back().AddInput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Target::Input::Signal));
+	_types.back().AddInput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Target::Input::Signal)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));;
 
 	_types.emplace_back("filt", "Filter", Engine::ModuleType::Filter);
 	_types.back().AddInput(std::make_unique<PinType>("freq", "Frequency", PinType::ConnectionType::Single, Engine::Pin::Filter::Input::Frequency)).SetValueType(std::make_unique<PercentValueType>(0, 100, 0, 100));
 	_types.back().AddInput(std::make_unique<PinType>("resn", "Resonance", PinType::ConnectionType::Single, Engine::Pin::Filter::Input::Resonance)).SetValueType(std::make_unique<PercentValueType>(0, 100, 0, 100));
 	_types.back().AddInput(std::make_unique<PinType>("byps", "Bypass", PinType::ConnectionType::Single, Engine::Pin::Filter::Input::Bypass)).SetValueType(std::make_unique<BoolValueType>(0));
-	_types.back().AddInput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Filter::Input::Signal));
+	_types.back().AddInput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Filter::Input::Signal)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
 	_types.back().AddOutput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Filter::Output::Signal));
 
 	_types.emplace_back("math", "Maths", Engine::ModuleType::Maths);
-	_types.back().AddInput(std::make_unique<PinType>("valu", "Value", PinType::ConnectionType::Single, Engine::Pin::Maths::Input::Value));
+	_types.back().AddInput(std::make_unique<PinType>("valu", "Value", PinType::ConnectionType::Single, Engine::Pin::Maths::Input::Value)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100000000, 100000000));
 	_types.back().AddInput(std::make_unique<PinType>("add_", "Add", PinType::ConnectionType::Single, Engine::Pin::Maths::Input::Add)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100000000, 100000000));
 	_types.back().AddInput(std::make_unique<PinType>("mult", "Multiply", PinType::ConnectionType::Single, Engine::Pin::Maths::Input::Multiply)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100000000, 100000000));
 	_types.back().AddOutput(std::make_unique<PinType>("valu", "Value", PinType::ConnectionType::Single, Engine::Pin::Maths::Output::Value));
 
 	_types.emplace_back("mixr", "Mixer", Engine::ModuleType::Mixer);
-	_types.back().AddInput(std::make_unique<PinType>("sig0", "Signal 0", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal0));
-	_types.back().AddInput(std::make_unique<PinType>("sig1", "Signal 1", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal1));
-	_types.back().AddInput(std::make_unique<PinType>("sig2", "Signal 2", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal2));
-	_types.back().AddInput(std::make_unique<PinType>("sig3", "Signal 3", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal3));
+	_types.back().AddInput(std::make_unique<PinType>("sig0", "Signal 0", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal0)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig1", "Signal 1", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal1)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig2", "Signal 2", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal2)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig3", "Signal 3", PinType::ConnectionType::Single, Engine::Pin::Mixer::Input::Signal3)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
 	_types.back().AddOutput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Mixer::Output::Signal));
 
 	_types.emplace_back("mult", "Multiply", Engine::ModuleType::Multiply);
-	_types.back().AddInput(std::make_unique<PinType>("sig0", "Signal 0", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal0));
-	_types.back().AddInput(std::make_unique<PinType>("sig1", "Signal 1", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal1));
-	_types.back().AddInput(std::make_unique<PinType>("sig2", "Signal 2", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal2));
-	_types.back().AddInput(std::make_unique<PinType>("sig3", "Signal 3", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal3));
+	_types.back().AddInput(std::make_unique<PinType>("sig0", "Signal 0", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal0)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig1", "Signal 1", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal1)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig2", "Signal 2", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal2)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
+	_types.back().AddInput(std::make_unique<PinType>("sig3", "Signal 3", PinType::ConnectionType::Single, Engine::Pin::Multiply::Input::Signal3)).SetValueType(std::make_unique<PercentValueType>(0, 100, -100, 100));
 	_types.back().AddOutput(std::make_unique<PinType>("sgnl", "Signal", PinType::ConnectionType::Single, Engine::Pin::Multiply::Output::Signal));
 
 	_types.emplace_back("lfo", "LFO", Engine::ModuleType::LFO);
